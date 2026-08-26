7b2e0dd10054616b087d7cad1e5d49ade62be95b
Saeed Mohamed <sydalshghty15@gmail.com>
2026-08-26 14:49:55 +0300

    update-new-changes

--- src/components/hero-section.jsx
+++ src/components/hero-section.jsx
@@ -20,7 +20,7 @@ function HeroSection() {
                     <div className="col-btns flex flex-row-reverse gap-4">
                         <Link to={`/ordernow`} className="flex flex-row-reverse justify-center items-center gap-2 md:gap-4 w-36 h-11 md:w-46.25 md:h-15 rounded-xl bg-(--textcolor1)">
                             <p className="text-[12px] md:text-[14px] text-white font-medium">احجز موعدك الآن</p>
-                            <ArrowLeft className="text-white text-[14px] md:text-[17px] mt-1" />
+                            <ArrowLeft className="text-white w-4 md:w-6 mt-1" />
                         </Link>
                         <button className="w-32 h-11 md:w-43 md:h-15 border border-(--textcolor1) rounded-xl cursor-pointer text-[12px] md:text-[16px] text-(--textcolor1) font-medium hover:bg-(--textcolor1) hover:text-white transition-colors">
                             استكشف خدماتنا
